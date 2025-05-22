2d5506845dd8638afef8243a5bba59736e864f40
Wasuchok <116450019+wasuchok@users.noreply.github.com>
2025-05-22 10:29:54 +0700

    Update

--- src/controller/hardware-controller.js
+++ src/controller/hardware-controller.js
@@ -1,26 +1,44 @@
 const WebSocket = require("ws");
 const mqtt = require("mqtt");
-
+const url = require("url");
 exports.getValueHardware = async (req, res) => {
   const wss = new WebSocket.Server({ port: 5001 });
   const mqttClient = mqtt.connect("mqtt://103.245.167.111:1883");
 
-  const topic = "id-002/tx";
+  //const topic = "id-002/tx";
+
+  const clientTopics = new Map();
 
   mqttClient.on("connect", () => {
-    console.log("✅ MQTT connected on port 1883");
-    mqttClient.subscribe(topic);
+    console.log("✅ MQTT connected");
   });
 
   mqttClient.on("message", (topic, message) => {
+    const msg = message.toString();
     wss.clients.forEach((ws) => {
-      if (ws.readyState === WebSocket.OPEN) {
-        ws.send(message.toString());
+      if (ws.readyState === WebSocket.OPEN && clientTopics.get(ws) === topic) {
+        ws.send(msg);
       }
     });
   });
 
-  wss.on("connection", (ws) => {
-    console.log("🌐 React client connected");
+  wss.on("connection", (ws, req) => {
+    const params = new URLSearchParams(url.parse(req.url).query);
+    const topic = params.get("topic");
+
+    if (topic) {
+      clientTopics.set(ws, topic);
+      mqttClient.subscribe(topic, (err) => {
+        if (err) {
+          console.error("❌ Subscribe failed:", topic);
+        } else {
+          console.log(`📡 Subscribed to ${topic}`);
+        }
+      });
+    }
+
+    ws.on("close", () => {
+      clientTopics.delete(ws);
+    });
   });
 };
